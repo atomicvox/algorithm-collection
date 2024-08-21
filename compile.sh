43e74439381626e0b40a53c8a80227511f0db7db
atomicvox <178934013+atomicvox@users.noreply.github.com>
2024-08-20 21:01:01 -0400

    Wrong compile.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+#gcc yellowstoneAlg.c -lm -o yellowstoneAlg.out
+#gcc yellowstoneAlgEuclidian.c -lm -o yellowstoneAlgE.out
+#gcc pi.c -fopenmp -o pi.out
+#gcc piMonteCarlo.c -lm -fopenmp -o piMonteCarlo.out
+#gcc pi.c -lm -fopenmp -o pi.out
+#gcc fibanachi.c  -o fibonachi.out
+#gcc test.c -o test.out
 gcc gravler.c -fopenmp -o gravler.out
-
-./gravler.out 10000
-./gravler.out 50000
-./gravler.out 100000
-./gravler.out 200000
+#gcc rand_test.c -o rand_test.out -lm
